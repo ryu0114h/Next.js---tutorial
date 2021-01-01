5127a70548c600729645c567e0fc3e9f616e36ec
ryu0114h <ryu0114h@gmail.com>
2021-01-02 02:46:14 +0900

    Pre-rendering and Data Fetching/Dynamic Routes

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Ryu Hoshino";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Next.js Website";
 
 export default function Layout({ children, home }) {
   return (
